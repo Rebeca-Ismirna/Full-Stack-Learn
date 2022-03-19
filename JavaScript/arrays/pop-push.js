802ef6e1a58f0d8d88b50225ce51fd9fc3d65f5d
Rebeca Ismirna <rebecaismirna10@gmail.com>
2022-03-18 23:50:57 -0300

    up

---
+++ JavaScript/arrays/pop-push.js
@@ -0,0 +1,13 @@
+// pop() -> remove the last element of an array
+
+const nomes = ['Rebeca', 'Ismirna', 'Navegantes', 'de', 'Melo']
+const nomeremovido = nomes.pop()
+
+console.log(nomes, nomeremovido)
+
+// push() -> add an element at the last index of an array
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+const addNumber = numeros.push(9)
+
+console.log(numeros, addNumber)
